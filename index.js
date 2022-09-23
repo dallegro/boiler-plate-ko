10997d1f2ee7e8e431489142f2867dca352828bb
Daseul Kim <kimds1599@naver.com>
2022-09-23 10:07:19 +0900

    처음 저장소에 올림

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose= require('mongoose')
+mongoose.connect('mongodb+srv://kimds1599:dsdy1599@boilerplate.m3kbccr.mongodb.net/?retryWrites=true&w=majority', {
+    // useNewUrlParser: true, 
+    // useUnifiedTopology: true, 
+    // useCreateIndex: true, 
+    // useFindAndModify: false,
+}).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.error(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World! 안녕하세요~~!!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

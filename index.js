00fd811524a78221cce7f51e82c03ca88c63110d
dallegro <80038703+dallegro@users.noreply.github.com>
2022-09-23 14:46:07 +0900

    회원가입, 로그인 추가

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ app.post("/login", (req, res) => {
 				message: "제공된 이메일에 해당하는 유저가 없습니다.",
 			});
 		}
-		//요청된 이메일이 데이터 베이스에 있다면 비밈ㄹ번호가 맞는 비밀번호 인지 확인.
+		//요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
 		user.comparePassword(req.body.comparePassword, (err, isMatch) => {
 			if (!isMatch)
 				return res.json({

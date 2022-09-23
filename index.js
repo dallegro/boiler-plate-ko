182f4595a59a939fb85b68054a67836af39cb6c2
dallegro <80038703+dallegro@users.noreply.github.com>
2022-09-23 14:45:34 +0900

    회원가입, 로그인 추가

--- index.js
+++ index.js
@@ -1,43 +1,67 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const bodyParer = require('body-parser');
+const express = require("express");
+const app = express();
+const port = 3000;
+const bodyParer = require("body-parser");
 
-const config = require('./config/key');
+const config = require("./config/key");
 
-const { User } = require('./models/User');
+const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
 app.use(bodyParer.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParer.json());
 
+const mongoose = require("mongoose");
+mongoose
+	.connect(config.mongoURI, {
+		// useNewUrlParser: true,
+		// useUnifiedTopology: true,
+		// useCreateIndex: true,
+		// useFindAndModify: false,
+	})
+	.then(() => console.log("MongoDB Connected..."))
+	.catch((err) => console.error(err));
 
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
 
-const mongoose= require('mongoose')
-mongoose.connect(config.mongoURI, {
-    // useNewUrlParser: true, 
-    // useUnifiedTopology: true, 
-    // useCreateIndex: true, 
-    // useFindAndModify: false,
-}).then(() => console.log('MongoDB Connected...'))
-    .catch(err => console.error(err))
+app.post("/register", (req, res) => {
+	//회원 가입 할 때 필요한 정보들을 client에서 가져오면
+	//그것들을 데이터 베이스에 넣어준다.
+	const user = new User(req.body);
 
-app.get('/', (req, res) => { res.send('Hello World!') })
+	user.save((err, userInfo) => {
+		if (err) return res.json({ sucess: false, err });
+		return res.status(200).json({
+			sucess: true,
+		});
+	});
+});
 
-app.post('/register', (req, res) => {
-  //회원 가입 할 때 필요한 정보들을 client에서 가져오면 
-  //그것들을 데이터 베이스에 넣어준다.
-  const user = new User(req.body)
-
-  user.save((err, userInfo) => {
-    if (err) return res.json({ sucess: false, err })
-    return res.status(200).json({
-      sucess: true
-    })
-  })
-})
+app.post("/login", (req, res) => {
+	//요청된 이메일을 데이터베이스에서 있는지 찾는다.
+	User.findOne({ email: req.body.email }, (err, user) => {
+		if (!user) {
+			return res.json({
+				loginSucess: false,
+				message: "제공된 이메일에 해당하는 유저가 없습니다.",
+			});
+		}
+		//요청된 이메일이 데이터 베이스에 있다면 비밈ㄹ번호가 맞는 비밀번호 인지 확인.
+		user.comparePassword(req.body.comparePassword, (err, isMatch) => {
+			if (!isMatch)
+				return res.json({
+					loginSucess: false,
+					message: "비밀번호가 틀렸습니다.",
+				});
+			//비밀번호 까지 맞다면 Token을 생성하기.
+			user.generateToken((err, user) => {});
+		});
+	});
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+	console.log(`Example app listening on port ${port}!`);
+});
--- models/User.js
+++ models/User.js
@@ -1,36 +1,64 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const userSchema = mongoose.Schema({
-    name: {
-        type: String,
-        maxlength:50
-    },
-    email: {
-        type: String,
-        trim: true,
-        unique: 1
-    },
-    password: {
-        type: String,
-        maxlength:50
-    },
-    lastname: {
-        type: String,
-        maxlength: 5
-    },
-    role: {
-        type: Number,
-        default: 0,    
-    },
-    image: String,
-    token: {
-        type: String
-    },
-    tokenExp: {
-        type : Number
-    }
-})
+	name: {
+		type: String,
+		maxlength: 50,
+	},
+	email: {
+		type: String,
+		trim: true,
+		unique: 1,
+	},
+	password: {
+		type: String,
+		maxlength: 50,
+	},
+	lastname: {
+		type: String,
+		maxlength: 5,
+	},
+	role: {
+		type: Number,
+		default: 0,
+	},
+	image: String,
+	token: {
+		type: String,
+	},
+	tokenExp: {
+		type: Number,
+	},
+});
 
-const User = mongoose.model('User', userSchema)
+userSchema.pre("save", function (next) {
+	var user = this;
 
-module.exports = { User }
+	if (user.isModified("password")) {
+		//비밀번호를 암호화 시킨다.
+		bcrypt.genSalt(saltRounds, function (err, salt) {
+			if (err) return next(err);
+
+			bcrypt.hash(user.password, salt, function (err, hash) {
+				if (err) return next(err);
+				user.password = hash;
+				next();
+			});
+		});
+	} else {
+		next();
+	}
+});
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+	//plainPassword 1234567 암호화된 비밀번호 $2b$10$7W/hQMLrrnx9VhIkwN/qMOM1DMCLRJpyQNKLfvwLD2aS5R4icaYkS
+	bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+		if (err) return cb(err), cb(null, isMatch);
+	});
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = { User };
